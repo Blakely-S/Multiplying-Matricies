3916021fb0667d5e076d393da6425435746ffb70
Blakely-S <blakelyshreeber@gmail.com>
2024-05-18 23:04:07 -0700

    t7oltiultil

--- cool_math.py
+++ cool_math.py
@@ -1,2 +1,17 @@
-m1 = input("insert first matrix here:/n")
-print(m1)
+
+from flask import Flask, request, render_template 
+  
+# Flask constructor
+app = Flask(__name__)   
+  
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/cool_math.py', methods =["GET", "POST"])
+def cool_math():
+    if request.method == "POST":
+       # getting input with freq = set_freq in HTML form
+       matricies = request.form.get("matrix1")
+       return "Your freq value is " + matrix
+    return render_template("index.html")
+if __name__=='__main__':
+   app.run()
