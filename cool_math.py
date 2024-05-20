2df3c480768b3f6e3a8a7a0c53bb6956b68abec1
CyBoy <brennanlafave@gmail.com>
2024-05-20 14:10:52 -0700

    Making it bettar

--- cool_math.py
+++ cool_math.py
@@ -1,23 +1,26 @@
-#Inputting Matrix
-Row1 = int(input("Give the number of rows:"))  
-Column1 = int(input("Give the number of columns:"))  
-matrix1 = [[int(input())for c in range (Column1)] for r in range(Row1)]
+a=1
+while a<10:
+    #Inputting Matrix
+    Row1 = int(input("Give the number of rows:"))  
+    Column1 = int(input("Give the number of columns:"))  
+    matrix1 = [[int(input())for c in range (Column1)] for r in range(Row1)]
 
+    Row2 = int(input("Give the number of rows:"))  
+    Column2 = int(input("Give the number of columns:"))  
+    matrix2 = [[int(input())for c in range (Column2)] for r in range(Row2)]
+    print("first matrix: " + str(matrix1)) 
+    print(" ")
+    print("second matrix: " + str(matrix2))
 
-Row2 = int(input("Give the number of rows:"))  
-Column2 = int(input("Give the number of columns:"))  
-matrix2 = [[int(input())for c in range (Column2)] for r in range(Row2)]
-print("first matrix: " + str(matrix2)) 
-print("second matrix: " + str(matrix1))   
+    #Multiplying Matrix
+    def multiply(m1,m2):
+        result = [[0 for _ in range(len(m2[0]))] for _ in range(len(m1))]
 
-#Multiplying Matrix
-def multiply(m1,m2):
-    result = [[0 for _ in range(len(m2[0]))] for _ in range(len(m1))]
+        for i in range(len(m1)):
+           for j in range(len(m2[0])):
+               for k in range(len(m2)):
+                   result[i][j] += m1[i][k] * m2[k][j]
 
-    for i in range(len(m1)):
-        for j in range(len(m2[0])):
-            for k in range(len(m2)):
-                result[i][j] += m1[i][k] * m2[k][j]
-
-    return result
-print("resulting matrix" + str(multiply(matrix1,matrix2)))
+        return result
+    print("")
+    print("resulting matrix" + str(multiply(matrix1,matrix2)))
